83ae9560ed1c901af605c47380e06061e45cbc98
Xu Shane <xusheng0711@gmail.com>
2019-09-26 22:27:51 +0800

    sort with binary search

--- ch02/isort/isort.go
+++ ch02/isort/isort.go
@@ -7,3 +7,30 @@ func ISort(xs []int) {
 		}
 	}
 }
+
+func BinarySearch(xs []int, x int) int {
+	l := 0
+	u := len(xs)
+	for l < u {
+		m := (l + u) / 2
+		if xs[m] == x {
+			return m
+		} else if xs[m] < x {
+			l = m + 1
+		} else {
+			u = m
+		}
+	}
+	return l
+}
+
+func ISortB(xs []int) {
+	for i := range xs {
+		x := xs[i]
+		p := BinarySearch(xs[:i], x)
+		for j := i; j >= 1; j-- {
+			xs[j] = xs[j-1]
+		}
+		xs[p] = x
+	}
+}
--- ch02/isort/isort_test.go
+++ ch02/isort/isort_test.go
@@ -43,3 +43,92 @@ func TestISort(t *testing.T) {
 		})
 	}
 }
+
+func TestBinarySearch(t *testing.T) {
+	type args struct {
+		xs []int
+		x  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"nil",
+			args{nil, 1},
+			0,
+		},
+		{
+			"single",
+			args{[]int{1}, 1},
+			0,
+		},
+		{
+			"single2",
+			args{[]int{1}, 0},
+			0,
+		},
+		{
+			"single3",
+			args{[]int{1}, 2},
+			1,
+		},
+		{
+			"two",
+			args{[]int{1, 2}, 2},
+			1,
+		},
+		{
+			"two1",
+			args{[]int{1, 2}, 3},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BinarySearch(tt.args.xs, tt.args.x); got != tt.want {
+				t.Errorf("BinarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestISortB(t *testing.T) {
+	type args struct {
+		xs []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"nil",
+			args{nil},
+			nil,
+		},
+		{
+			"single",
+			args{[]int{1}},
+			[]int{1},
+		},
+		{
+			"two",
+			args{[]int{2, 1}},
+			[]int{1, 2},
+		},
+		{
+			"three",
+			args{[]int{2, 3, 1}},
+			[]int{1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if ISortB(tt.args.xs); !reflect.DeepEqual(tt.args.xs, tt.want) {
+				t.Errorf("ISort() = %v, want %v", tt.args.xs, tt.want)
+			}
+		})
+	}
+}
