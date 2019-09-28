19bb195d60a28c0d1ee2240fc722ac3a1b5d3a08
Xu Shane <xusheng0711@gmail.com>
2019-09-28 10:11:04 +0800

    left right rotate

--- ch03/rbt/rbt.go
+++ ch03/rbt/rbt.go
@@ -4,26 +4,65 @@ import (
 	. "github.com/shanexu/go-elementary-algorithms/tree"
 )
 
-func LeftRotate(t *Node, x *Node) *Node {
-	y := x.Right
-	b := y.Left
+func LeftRotate(t, x *Node) *Node {
 	p := x.Parent
-	x.Right = b
-	b.Parent = x
-	x.Parent = y
-	y.Left = x
-	y.Parent = p
+	y := x.Right
+	a := x.Left
+	b := y.Left
+	c := y.Right
+	Replace(x, y)
+	SetChildren(x, a, b)
+	SetChildren(y, x, c)
 	if p == nil {
-		return y
-	}
-	if p.Left == x {
-		p.Left = y
-	} else {
-		p.Right = y
+		t = y
 	}
 	return t
 }
 
-func RightRotate(t *Node, x *Node) *Node {
-	panic("// TODO") // TODO
+func RightRotate(t, y *Node) *Node {
+	p := y.Parent
+	x := y.Left
+	a := x.Left
+	b := x.Right
+	c := y.Right
+	Replace(y, x)
+	SetChildren(y, b, c)
+	SetChildren(x, a, y)
+	if p == nil {
+		t = x
+	}
+	return t
 }
+
+func Replace(x, y *Node) {
+	if x.Parent == nil {
+		if y != nil {
+			y.Parent = nil
+		}
+	} else if x.Parent.Left == x {
+		SetLeft(x.Parent, y)
+	} else {
+		SetRight(x.Parent, y)
+	}
+	x.Parent = nil
+}
+
+func SetChildren(x, l, r *Node) {
+	SetLeft(x, l)
+	SetRight(x, r)
+}
+
+func SetLeft(x, y *Node) {
+	x.Left = y
+	if y != nil {
+		y.Parent = x
+	}
+}
+
+func SetRight(x, y *Node) {
+	x.Right = y
+	if y != nil {
+		y.Parent = x
+	}
+}
+
